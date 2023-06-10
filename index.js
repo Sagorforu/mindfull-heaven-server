a9b291cdbfc5f2140a12d6937ca4b439c6097679
sagorforu <freelancersagor18@gmail.com>
2023-06-10 20:46:34 +0600

    jwt token create and send and error fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -31,7 +32,7 @@ async function run() {
     app.post("/jwt", (req, res)=> {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
-      res.send(token);
+      res.send({token});
     })
 
     // users related api
