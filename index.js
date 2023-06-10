8ca2402c5ec19328211a7acc050e41f1c0aff953
sagorforu <freelancersagor18@gmail.com>
2023-06-11 00:36:45 +0600

    isAdmin check route done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-// middlewares
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -79,6 +79,16 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+    app.get("/users/admin/:email", jwtVerify, async(req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({admin: false})
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    })
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
