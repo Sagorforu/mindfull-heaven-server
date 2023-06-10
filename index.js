744aeff348b7332ee213d9f8585e4f422b5fca25
sagorforu <freelancersagor18@gmail.com>
2023-06-10 19:37:12 +0600

    make instructor role added

--- index.js
+++ index.js
@@ -58,6 +58,17 @@ async function run() {
       const result = await usersCollection.updateOne(query, updateDoc);
       res.send(result);
     });
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
