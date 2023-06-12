5c8665007b1ec9a575a81827204578bffbcf0289
sagorforu <freelancersagor18@gmail.com>
2023-06-12 08:48:40 +0600

    payment by stripe method implement done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -50,6 +51,7 @@ async function run() {
 
     const usersCollection = client.db("mindFullHeaven").collection("users");
     const classesCollection = client.db("mindFullHeaven").collection("classes");
+    const paymentCollection = client.db("mindFullHeaven").collection("payments");
     const selectedClassesCollection = client
       .db("mindFullHeaven")
       .collection("selectedClasses");
@@ -246,6 +248,41 @@ async function run() {
       const query = { email: email};
       const result = await selectedClassesCollection.find(query).toArray();
       res.send(result);
+    })
+    app.delete("/selectedClass/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassesCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // create payment intent
+    app.post("/create-payment-intent", jwtVerify, async(req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ["card"]
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    // payment related api
+    app.post("/payments", jwtVerify, async(req, res)=>{
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+      res.send( insertResult );
+    })
+    app.delete("/payments", jwtVerify, async(req, res)=>{
+      const payment = req.body;
+      const query = {
+        _id: { $in: payment.classesId.map((id) => new ObjectId(id)) },
+      };
+      const deleteResult = await selectedClassesCollection.deleteMany(query);
+      res.send( deleteResult );
     })
 
     // Send a ping to confirm a successful connection
